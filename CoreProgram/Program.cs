5cf3e285d3585c1deaa0d90f9ea439c8ae37460a
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-10 03:00:14 +0530

    all program at once

--- CoreProgram/Program.cs
+++ CoreProgram/Program.cs
@@ -8,35 +8,55 @@ namespace CoreProgram
         {
 
 
-            //FlipCoin flip = new FlipCoin();
-            //flip.Flip_coin();
+            Console.WriteLine("1. Flip Coin");
 
-            //LeapYear leap = new LeapYear();
-            //leap.Leap_Year();
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-            //Power2 power = new Power2();
-            //power.Power_of_two();
-
-            //Harmonic harmonic = new Harmonic();
-            //harmonic.Harmonics();
-
-            //Factors fac = new Factors();
-            //fac.factors();
-
-            //QuotientRemainder qr = new QuotientRemainder();
-            //qr.Quotient_Remainder();
-
-            //Swap swp = new Swap();
-            //swp.swap();
-
-            //EvenOdd EO = new EvenOdd();
-            //EO.evenodd();
-
-            //VowelConsonants chr = new VowelConsonants();
-            //chr.checkVowel();
-
-            LargestIn3 max = new LargestIn3();
-            max.largest();
+            switch (choice)
+            {
+                case 1:
+                    FlipCoin flip = new FlipCoin();
+                    flip.Flip_coin();
+                    break;
+                case 2:
+                    LeapYear leap = new LeapYear();
+                    leap.Leap_Year();
+                    break;
+                case 3:
+                    Power2 power = new Power2();
+                    power.Power_of_two();
+                    break;
+                case 4:
+                    Harmonic harmonic = new Harmonic();
+                    harmonic.Harmonics();
+                    break;
+                case 5:
+                    Factors fac = new Factors();
+                    fac.factors();
+                    break;
+                case 6:
+                    QuotientRemainder qr = new QuotientRemainder();
+                    qr.Quotient_Remainder();
+                    break;
+                case 7:
+                    Swap swp = new Swap();
+                    swp.swap();
+                    break;
+                case 8:
+                    EvenOdd EO = new EvenOdd();
+                    EO.evenodd();
+                    break;
+                case 9:
+                    VowelConsonants chr = new VowelConsonants();
+                    chr.checkVowel();
+                    break;
+                case 10:
+                    LargestIn3 max = new LargestIn3();
+                    max.largest();
+                    break;
+                default:Console.WriteLine("Invalid input. Input 1-10");
+                    break;
+            }
         }
     }
 }
